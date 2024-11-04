56abca30bb733b250bdc2838c64474b9cf941c6d
Sean Wang <seanwangtech@gmail.com>
2024-11-04 03:18:15 +0000

    Initial version running through

--- ddqn_rl.py
+++ ddqn_rl.py
@@ -178,14 +178,16 @@ import random
 from collections import deque
 
 class DDQNAgent:
-    def __init__(self, action_space, model, optimizer, loss_fn, gamma=0.99, device='cpu'):
+    def __init__(self, action_space, policy_net, target_net, optimizer, loss_fn, gamma=0.99, device='cpu'):
         self.action_space = action_space
-        self.model = model
         self.optimizer = optimizer
         self.loss_fn = loss_fn
         self.gamma = gamma
         self.memory = deque(maxlen=2000)
-        self.target_model = model  # Set target model for DDQN
+        self.policy_net = policy_net
+        self.target_net = target_net  # Set target model for DDQN
+        self.target_net.load_state_dict(policy_net.state_dict())
+        self.target_net.eval()
         self.epsilon = 1.0  # Epsilon-greedy action selection
         self.epsilon_decay = 0.995
         self.epsilon_min = 0.1
@@ -193,32 +195,61 @@ class DDQNAgent:
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
-
-    def act(self, state):
+    
+    def update_epsilon(self):
+        self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+        
+    def update_target_net(self):
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+    
+    def select_action(self, state):
         if random.random() <= self.epsilon:
-            return random.choice(range(self.action_space.n))
-        state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+            return self.action_space.sample() # Explore
         with torch.no_grad():
-            q_values = self.model(state)
-        return q_values.argmax().item()
+            state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+            q_values = self.policy_net(state)
+        return q_values.argmax().item()   # Exploit
 
     def replay(self, batch_size=32):
         if len(self.memory) < batch_size:
             return
         batch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in batch:
-            target = reward
-            if not done:
-                next_state = torch.FloatTensor(next_state).unsqueeze(0).to(self.device)
-                target += self.gamma * self.target_model(next_state).max(1)[0].item()
-            target_f = self.model(torch.FloatTensor(state).unsqueeze(0).to(self.device))
-            target_f[0][action] = target
-            loss = self.loss_fn(target_f, torch.FloatTensor(target).unsqueeze(0).to(self.device))
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+        states, actions, rewards, next_states, dones = zip(*batch)
+        states = torch.stack(states).to(self.device)
+        actions = torch.LongTensor(actions).unsqueeze(1).to(self.device)
+        rewards = torch.FloatTensor(rewards).to(self.device)
+        next_states = torch.stack(next_states).to(self.device)
+        dones = torch.FloatTensor(dones).to(self.device)
+        
+        # Compute current Q values
+        current_q_values = self.policy_net(states).gather(1, actions)
+        # Double DQN: Use policy net to choose action, target net to calculate Q-value of that action
+        with torch.no_grad():
+            # Get the best action from the policy network
+            next_actions = self.policy_net(next_states).argmax(1, keepdim=True)
+            # Use target network to get Q-values for these actions
+            next_q_values = self.target_net(next_states).gather(1, next_actions).squeeze()
+            target_q_values = rewards + (self.gamma * next_q_values * (1 - dones))
+        
+        loss = self.loss_fn(current_q_values.squeeze(), target_q_values)
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        
+        # for state, action, reward, next_state, done in batch:
+        #     target = reward
+        #     if not done:
+        #         next_state = torch.FloatTensor(next_state).unsqueeze(0).to(self.device)
+        #         target += self.gamma * self.target_model(next_state).max(1)[0].item()
+        #     target_f = self.model(torch.FloatTensor(state).unsqueeze(0).to(self.device))
+        #     target_f[0][action] = target
+        #     loss = self.loss_fn(target_f, torch.FloatTensor(target).unsqueeze(0).to(self.device))
+        #     self.optimizer.zero_grad()
+        #     loss.backward()
+        #     self.optimizer.step()
+        # if self.epsilon > self.epsilon_min:
+        #     self.epsilon *= self.epsilon_decay
             
             
 import torch.nn.functional as F
@@ -227,20 +258,24 @@ import time
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 env = BlackMythWukongEnv()
-model = DQNEfficientNet(env.action_space.n)
+policy_net = DQNEfficientNet(env.action_space.n)
+target_net = DQNEfficientNet(env.action_space.n)
 loss_fn = F.mse_loss
 # Assuming `model` is an instance of DQNEfficientNet and learning_rate is defined
 learning_rate = 1e-4
 
 # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 optimizer = torch.optim.Adam([
-    {'params': model.features.parameters(), 'lr': 1e-5},  # Low LR for pre-trained backbone
-    {'params': model.fc.parameters(), 'lr': 1e-3}  # Higher LR for classifier
+    {'params': policy_net.features.parameters(), 'lr': 1e-5},  # Low LR for pre-trained backbone
+    {'params': policy_net.fc.parameters(), 'lr': 1e-3}  # Higher LR for classifier
 ],lr=0.001)
 # for param in model.features.parameters():
 #     param.requires_grad = False  # Freeze pre-trained layers
 
-agent = DDQNAgent(env.action_space, model, optimizer, loss_fn, device=device)
+agent = DDQNAgent(env.action_space, 
+                  policy_net=policy_net,
+                  target_net=target_net,
+                  optimizer=optimizer, loss_fn=loss_fn, device=device)
 
 episodes = 1000
 batch_size = 32
@@ -261,7 +296,7 @@ for episode in range(episodes):
     total_reward = 0
     
     while not done:
-        action = agent.act(state)
+        action = agent.select_action(state)
         obs_img, reward, done, info = env.step(action)
         if(not info['pasued']):
             # not pased
@@ -271,7 +306,8 @@ for episode in range(episodes):
             agent.remember(state, action, reward, next_state, done)
             state = next_state
             agent.replay(batch_size)
-        # Display real-time window  r
+            agent.update_epsilon()
+        # Display real-time window 
         if(frame_count%20 == 0):
             print(obs_img.shape)
             img = obs_img 
